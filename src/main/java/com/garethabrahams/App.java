b1ce3433e173d8c9bd18474d23b412973b221ef9
Gareth Abrahams <garetha6@gmail.com>
2019-02-12 23:04:14 +0200

    second commit

---
+++ src/main/java/com/garethabrahams/App.java
@@ -0,0 +1,13 @@
+package com.garethabrahams;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
